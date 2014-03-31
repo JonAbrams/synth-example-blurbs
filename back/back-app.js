4e8365f5013727f8b73974e7a51fdf3b4865b5ab
Jon Abrams <jon@jonabrams.com>
2014-03-30 23:13:47 -0700

    Remove pointless middleware

--- back/back-app.js
+++ back/back-app.js
@@ -3,12 +3,6 @@ var synth = require('synth');
 /* Connect to Mongo DB */
 var db = require('promised-mongo')(process.env.MONGODB || 'synth-default-db');
 
-/* Define your middleware here */
-synth.app.use(function (req, res, next) {
-  req.appName = "bigboard";
-  next();
-});
-
 /* Provide DB connection to request handlers */
 synth.app.use(function (req, res, next) {
   req.db = db;
