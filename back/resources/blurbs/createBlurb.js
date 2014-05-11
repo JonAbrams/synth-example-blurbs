b8245583f4aaf97bcb84732d67d09cd108096a0a
Jon Abrams <jon@jonabrams.com>
2014-05-11 16:38:16 -0700

    Throw errors instead of talking to Express

--- back/resources/blurbs/createBlurb.js
+++ back/resources/blurbs/createBlurb.js
@@ -1,9 +1,13 @@
 /* Create a new blurb entry in the db */
 exports.post = function (req, res) {
   var message = req.body.message;
-  if (typeof message !== 'string' && message.length === 0) {
-    return res.send(422, "Could not create blurb without a message");
+  if (typeof message !== 'string' || message.length === 0) {
+    throw {
+      statusCode: 422,
+      message: "Could not create blurb without a message"
+    };
   }
+
   return req.db.collection('blurbs').insert({
     message: message.slice(0,140),
     created_at: new Date(),
